cc64f381774b6964272c9ae9c8f07beb053e27c2
AnjanPrasad <pk.anjan1993@gmail.com>
2017-05-09 15:08:26 +0530

    First Modified

---
+++ SalesTool/src/com/controller/Main.java
@@ -0,0 +1,16 @@
+package com.controller;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+	 SalesData data=new SalesData();
+	 displayGreeting();
+	 data.display();
+
+	}
+	public static void displayGreeting(){
+		System.out.println("welcome to Anjan GIT");
+	}
+
+}
---
+++ SalesTool/src/com/controller/SalesData.java
@@ -0,0 +1,10 @@
+package com.controller;
+
+public class SalesData {
+ int[] data={0,2,14,5};
+ public void display(){
+	 for(int i:data)
+		 System.out.println("todo:"+data[i]);
+ }
+ 
+}
